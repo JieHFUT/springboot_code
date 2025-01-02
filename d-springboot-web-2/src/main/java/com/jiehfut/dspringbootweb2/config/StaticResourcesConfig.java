f6d9678c06aea5745278766d25807b601bb4d8f0
JieCHERY <9594523wrj@gmail.com>
2025-01-02 15:35:25 +0800

    springboot

--- d-springboot-web-2/src/main/java/com/jiehfut/dspringbootweb2/config/StaticResourcesConfig.java
+++ d-springboot-web-2/src/main/java/com/jiehfut/dspringbootweb2/config/StaticResourcesConfig.java
@@ -1,13 +1,16 @@
 package com.jiehfut.dspringbootweb2.config;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.concurrent.TimeUnit;
 
+// @EnableWebMvc // 使用该注解会禁止默认配置信息
 @Configuration
 public class StaticResourcesConfig implements WebMvcConfigurer {
 
@@ -15,16 +18,38 @@ public class StaticResourcesConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         // 保留了以前默认的配置
-        WebMvcConfigurer.super.addResourceHandlers(registry);
-        // 自己添加规则
+        // WebMvcConfigurer.super.addResourceHandlers(registry);
+
+        // 自己添加规则（原有的规则还在）
         registry.addResourceHandler("/static/**")
                 .addResourceLocations("classpath:/first/")
                 .addResourceLocations("classpath:/second/")
                 .addResourceLocations("classpath:/third/")
                 .setCacheControl(CacheControl.maxAge(12, TimeUnit.SECONDS));
-        
 
     }
+
+    /**
+     * 也可以不实现 WebMvcConfigurer 接口
+     *
+     * @Configuration
+     * public class StaticResourcesConfig {
+     *     @Bean
+     *     public WebMvcConfigurer webMvcConfigurer() {
+     *         return new WebMvcConfigurer() {
+     *             @Override
+     *             public void addResourceHandlers(ResourceHandlerRegistry registry) {
+     *                 registry.addResourceHandler("/static/**")
+     *                         .addResourceLocations("classpath:/first/")
+     *                         .addResourceLocations("classpath:/second/")
+     *                         .addResourceLocations("classpath:/third/")
+     *                         .setCacheControl(CacheControl.maxAge(12, TimeUnit.SECONDS));
+     *             }
+     *         };
+     *     }
+     * }
+     */
+
 
 
 }
