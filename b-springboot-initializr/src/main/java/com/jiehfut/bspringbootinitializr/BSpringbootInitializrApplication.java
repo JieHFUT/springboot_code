fa456fa2194c74b7093820a0e11db13bf1743f8b
JieCHERY <9594523wrj@gmail.com>
2025-05-07 23:39:11 +0800

    redis-jedis

--- b-springboot-initializr/src/main/java/com/jiehfut/bspringbootinitializr/BSpringbootInitializrApplication.java
+++ b-springboot-initializr/src/main/java/com/jiehfut/bspringbootinitializr/BSpringbootInitializrApplication.java
@@ -88,11 +88,13 @@ public class BSpringbootInitializrApplication {
      * 每一个自动配置类 ***AutoConfigration 类
      *      1.按需加载进来的给容器中使用 @Bean 放置了一对组件
      *      2.每个自动配置类，都可能有注解 @EnableConfigurationProperties({ServerProperties.class})
-     *        用来把配置文件中的指定前缀的属性封装到 ***Properties 属性类中，如此是服务器配置都是以 server 开头的配置，进行属性绑定
+     *        用来把配置文件中的指定前缀的属性封装b b到 ***Properties 属性类中，如此是服务器配置都是以 server 开头的配置，进行属性绑定
      * 说明这些自动配置类 ***AutoConfigration 类里面的一些核心属性都来自于 ***Properties 属性类中
      * ***Properties 属性类即是和配置文件进行绑定的，所以可以通过更改配置文件信息来修改配置信息
      * 2.主程序 @SpringBootApplication
      * 3.完成业务代码，无需关心各种整合
+     *
+     *
      */
 
 
