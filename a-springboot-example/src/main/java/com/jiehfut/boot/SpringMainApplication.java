3dc913cd1ff284f75e511c1a4508e057a138b273
JieHFUT <9594523wrj@gmail.com>
2025-07-10 00:11:24 +0800

    测试提交

--- a-springboot-example/src/main/java/com/jiehfut/boot/SpringMainApplication.java
+++ a-springboot-example/src/main/java/com/jiehfut/boot/SpringMainApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication // 标明这是 springboot 的主程序
 public class SpringMainApplication {
 
+    // 测试提交 git
     public static void main(String[] args) {
         SpringApplication.run(SpringMainApplication.class, args);
     }
